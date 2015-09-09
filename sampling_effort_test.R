4a8b38758df892c57c564044560eaeb83f1de1e4
Jorge Doña <Jorge-Dona@users.noreply.github.com>
2015-09-09 18:27:06 +0200

    Update sampling_effort_test.R

--- sampling_effort_test.R
+++ sampling_effort_test.R
@@ -5,34 +5,36 @@
 
 #Require
 
-library(spider)
-library(ggplot2)
+require(spider)
+require(ggplot2)
 
 #Preparing data
-#require A matrix of DNA sequences as csv file:
-##Very important## csvfile= Must have the first column with numeric values for species, one number per specie.
+#A matrix of DNA sequences as csv file:
+#### csvfile= Must have the first column with numeric values for species, one number per specie.
 dat <- read.dna("name.fas", format="fasta")    
 dat<-  as.character(dat)
 write.csv(dat, file = "MyData.csv")
 
 #Threshold spp influence
 
-#nspp= Number of species in the matrix
-#csvfile= "Name of" csv file 
-#it= Number of iterations
+##Arguments
 
-##At least requires more than 5 species for threshold calculations
+nspp= Number of species in the matrix
+csvfile= "Name of" csv file 
+it= Number of iterations
+#More than 5 species are required for threshold calculations using localminima
 
+###
 
-species <- function(csvfile, it) {
+species <- function(csvfile, it, nspp) {
   x1=read.csv(csvfile, header= TRUE, sep=",", comment.char="")
   j= as.matrix(x1)
   threshold=numeric()
   index=numeric()
       for (iteration in 1:it){
       c= j[,1]
-      A= sample(6:65, 1)
-      x <- sample(1:65,A, replace =FALSE)
+      A= sample(6:nspp, 1) 
+      x <- sample(1:nspp,A, replace =FALSE)
       r= subset(j, c %in% c(x))
       h= r[,-1]
       p=as.DNAbin(h)
